35a6d1f6d3f6491fbc81e66c95866267fa5f3eff
Russ Smith <russ@bashme.org>
2011-10-22 21:59:53 -0700

    Config format fix.

--- lib/generators/devise_password_sharing_extension/install_generator.rb
+++ lib/generators/devise_password_sharing_extension/install_generator.rb
@@ -7,8 +7,8 @@ module DevisePasswordSharingExtension
       desc "Install the devise security extension"
 
       def add_configs
-        inject_into_file "config/initializers/devise.rb", "\n  # ==> Password Sharing Extension" +
-        "  # config.geoip_database = /var/tmp/geoip.dat\n" +
+        inject_into_file "config/initializers/devise.rb", "\n  # ==> Password Sharing Extension\n" +
+        "  # config.geoip_database = '/var/tmp/geoip.dat'\n" +
         "  # config.sharing_time_frame = 1.hour\n" +
         "  # config.number_of_cities = 2\n" +
         "\n", :before => /end[ |\n|]+\Z/
