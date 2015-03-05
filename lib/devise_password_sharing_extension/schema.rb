89e82cc62ac522232c144fc2ff445efab6e1f9f9
Jeremy Woertink <jeremywoertink@gmail.com>
2015-03-05 11:03:45 -0800

    devise 2.1 removes the schema stuff

--- lib/devise_password_sharing_extension/schema.rb
+++ lib/devise_password_sharing_extension/schema.rb
@@ -1,9 +1,11 @@
-module DevisePasswordSharingExtension
-  module Schema
-    def password_sharing
-      apply_devise_schema :banned_for_password_sharing_at, DateTime, :default => false
+if Devise::VERSION < "2.1"
+  module DevisePasswordSharingExtension
+    module Schema
+      def password_sharing
+        apply_devise_schema :banned_for_password_sharing_at, DateTime, :default => false
+      end
     end
   end
-end
 
-Devise::Schema.send(:include, DevisePasswordSharingExtension::Schema)
+  Devise::Schema.send(:include, DevisePasswordSharingExtension::Schema)
+end
